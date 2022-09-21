e2422917d447bddd6ee645f09138495aa2ef373b
Harrison <harriwilson5534@gmail.com>
2022-09-21 14:04:58 -0400

    commit tree 5082e9da070bdeebb1fc8027352aa759e585d7bd

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+// let element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//     li.textContent = "Hi there!";
+//     console.log(li.textContent);
+//   }
+
+//   element.append(ul);
+
+const mainSelector = document.getElementById("main");
+mainSelector.remove();
+
+const newHeader = document.createElement("h1");
+//body.append(newHeader);
+newHeader.id = "victory";
+newHeader.textContent = "Harrison is the champion";
